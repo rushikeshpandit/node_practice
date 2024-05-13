2519685f6a6c877e38d0833510e16bfb38c00436
Rushikesh Pandit <rushikesh.d.pandit@gmail.com>
2024-05-13 19:42:45 +0530

    - Addp.js changes

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
-const http = require("http");
 const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const rootDir = require("./utils/path");
+
+const adminRoute = require("./routes/admin");
+const shopRoute = require("./routes/shop");
 
 const app = express();
-app.use((req, res, next) => {
-	console.log("in middleware");
-	next();
-});
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(rootDir, "public")));
+app.use("/admin", adminRoute);
+app.use("/user", shopRoute);
 
 app.use((req, res, next) => {
-	console.log("in another middleware");
-	next();
+	res.status(404).sendFile(path.join(rootDir, "views", "404.html"));
 });
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
